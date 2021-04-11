7631b5c1ecc0b3957f94b033bed0274cd1383c87
SavicStefan <50296686+SavicStefan@users.noreply.github.com>
2021-04-11 13:35:06 +0200

    Update Fenwik.cpp

--- Stefano/Data Structure/Fenwik.cpp
+++ Stefano/Data Structure/Fenwik.cpp
@@ -1,13 +1,13 @@
 ll dud[maxn];
-void upd(int v, ll val){
-	while(x <= maxn){
+void upd(int v, ll val) {
+	while(x <= maxn) {
 		dud[x] += val;
 		x += x&(-x);
 	}
 }
-ll query(int x){
+ll query(int x) {
 	ll sum = 0;
-	while(x > 0){
+	while(x > 0) {
 		sum += dud[x];
 		x -= x&(-x);
 	}
