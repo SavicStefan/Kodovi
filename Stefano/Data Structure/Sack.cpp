d2cba2804f82e155893568c022fdcc5d79a8b471
SavicStefan <50296686+SavicStefan@users.noreply.github.com>
2021-04-14 00:30:55 +0200

    Update Sack.cpp

--- Stefano/Data Structure/Sack.cpp
+++ Stefano/Data Structure/Sack.cpp
@@ -1,7 +1,9 @@
+// When you need to swap ordered_set, you need to do dt[x].swap(dt[y])
+
 void comb(int x, int y) {
 	if(sz(dt[x]) < sz(dt[y]))swap(dt[x], dt[y]);
 	for(auto c : dt[y]) {
-
+		// do smth
 	}
 }
 
