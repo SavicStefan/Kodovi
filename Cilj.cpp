c2c70323a04ce5275f129741eb31109f42afc174
SavicStefan <50296686+SavicStefan@users.noreply.github.com>
2021-09-05 23:23:59 +0200

    Update Cilj.cpp

--- Cilj.cpp
+++ Cilj.cpp
@@ -9,36 +9,36 @@
 #define pb push_back
 #define sz(a) (int)a.size()
 #define all(a) a.begin(), a.end()
-#define ff(i,a,b) for(int i=a;i<=b;i++)
-#define fb(i,b,a) for(int i=b;i>=a;i--)
+#define ff(i,a,b) for(int i = a; i <= b; i++)
+#define fb(i,b,a) for(int i = b; i >= a; i--)
 
 using namespace std;
 using namespace __gnu_pbds;
 typedef long long ll;
 typedef pair<int,int> pii;
-const int maxn = 1000005;
+const int mxN = 1000005;
 const int inf = 1e9 + 5;
 
 template<typename T>
 using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
- 
+
 // os.order_of_key(k) the number of elements in the os less than k
 // *os.find_by_order(k)  print the k-th smallest number in os(0-based)
 
 
 
-int main()
-{
-   	ios::sync_with_stdio(false);
-   	cin.tie(nullptr);
-   	cout.tie(nullptr);
-	
-   	return 0;
+int main(){
+   cin.tie(0)->sync_with_stdio(0);
+
+
+
+   return 0;
 }
 /**
 
 
 
 **/
+
 
 
