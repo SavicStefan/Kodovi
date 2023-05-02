7c0066f685d23493268027df5f3282eab3b62e9d
SavicStefan <50296686+SavicStefan@users.noreply.github.com>
2023-05-02 14:40:06 +0200

    Update Z_Function.cpp

--- Stefano/Data Structure/Z_Function.cpp
+++ Stefano/Data Structure/Z_Function.cpp
@@ -1,15 +1,13 @@
-vector<int> z_function(string a) {
-	int N = sz(a);
-	vector<int> z(N);
-	int L = 0, R = -1;
-	ff(i,0,N - 1) {
-		int j = (i > R ? 0 : min(z[i - L], R - i + 1));
-		while(i + j < N && a[i + j] == a[j])j += 1;
-		z[i] = j--;
-		if(i + j > R) {
-			R = i + j;
-			L = i;
-		}
-	}
-	return z;
+vector<int> z_function(const string& a) {
+    int N = sz(a); vector<int> z(N); int L = 0, R = 0;
+    for (int i = 1; i < N; ++i) {
+        int j = (i >= R ? 0 : min(z[i - L], R - i));
+        while (i + j < N && a[i + j] == a[j])j += 1;
+        z[i] = j;
+        if (i + j > R) {
+            L = i;
+            R = i + j;
+        }
+    }
+    return z;
 }
